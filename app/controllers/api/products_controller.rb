5d661991ed0eed748b1d16ad05c09c68a68918b0
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-03 12:41:29 -0500

    update patch action to include default values

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,9 +5,9 @@ class Api::ProductsController < ApplicationController
   end
 
   def show
-     the_id = params[:id]
-     @product = Product.find_by(id: the_id)
-     render 'show.json.jbuilder'
+    the_id = params[:id]
+    @product = Product.find_by(id: the_id)
+    render 'show.json.jbuilder'
   end
 
   def create
@@ -26,10 +26,10 @@ class Api::ProductsController < ApplicationController
     the_id = params[:id]
     @product = Product.find_by(id: the_id)
     #update the movie I want to update
-    @product.name = params[:input_name]
-    @product.price = params[:input_price]
-    @product.image_url = params[:input_image_url]
-    @product.description = params[:input_description]
+    @product.name = params[:input_name] || @product.name
+    @product.price = params[:input_price] || @product.price
+    @product.image_url = params[:input_image_url] || @product.image_url
+    @product.description = params[:input_description] || @product.input_description
     @product.save
     render 'show.json.jbuilder'
   end
