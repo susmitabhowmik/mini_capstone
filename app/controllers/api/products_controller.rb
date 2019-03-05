129be738814600610c16b0f2c1f8c50b8b0cb18f
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-04 20:10:58 -0500

    update controller functions to be compatible with front end

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -12,10 +12,10 @@ class Api::ProductsController < ApplicationController
 
   def create
     @product = Product.new({
-      name: params[:input_name],
-      price: params[:input_price],
-      image_url: params[:input_image_url],
-      description: params[:input_description],
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
       })
     @product.save
     render 'show.json.jbuilder'
@@ -26,10 +26,10 @@ class Api::ProductsController < ApplicationController
     the_id = params[:id]
     @product = Product.find_by(id: the_id)
     #update the movie I want to update
-    @product.name = params[:input_name] || @product.name
-    @product.price = params[:input_price] || @product.price
-    @product.image_url = params[:input_image_url] || @product.image_url
-    @product.description = params[:input_description] || @product.input_description
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.input_description
     @product.save
     render 'show.json.jbuilder'
   end
