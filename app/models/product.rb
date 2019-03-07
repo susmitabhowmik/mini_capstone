a53963439ebed5192a40053bd7bcd6c9d6143d23
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-06 22:28:00 -0500

    add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,26 @@
 class Product < ApplicationRecord
-def is_discounted?
-  if price < 10
-    p true
-  else
-    p false
-  end    
- end
 
- def tax
-   tax = price*0.09
-   p tax.round(2)
- end
+  validates :name, :price, presence: true
+  validates :name, uniqueness: true
+  validates :price, numericality: {greater_than: 0}
+  validates :description, length: {in: 10..500}
 
- def total
-  total = price + tax
-  p total
- end
+
+  def is_discounted?
+    if price < 10
+      p true
+    else
+      p false
+    end    
+  end
+
+  def tax
+    tax = price*0.09
+    p tax.round(2)
+  end
+
+  def total
+    total = price + tax
+    p total
+  end
 end
