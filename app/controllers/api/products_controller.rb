8ace93d9176323f5c78061d92ca66724e29efb66
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-09 21:00:21 -0500

    update post and patch requests to include in_stock attribute

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -15,7 +15,8 @@ class Api::ProductsController < ApplicationController
       name: params[:name],
       price: params[:price],
       image_url: params[:image_url],
-      description: params[:description]
+      description: params[:description],
+      in_stock: params[:in_stock]
       })
     if @product.save
       render 'show.json.jbuilder'
@@ -32,6 +33,7 @@ class Api::ProductsController < ApplicationController
     @product.price = params[:price] || @product.price
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
+    @product.in_stock = params[:in_stock] || @product.in_stock
     if @product.save
       render 'show.json.jbuilder'
     else
