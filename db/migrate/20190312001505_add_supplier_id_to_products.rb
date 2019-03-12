52c238865bf8f64d61f3cc8ac8d02cd6ee832a16
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-11 20:16:28 -0400

    add Supplier model

---
+++ db/migrate/20190312001505_add_supplier_id_to_products.rb
@@ -0,0 +1,4 @@
+class AddSupplierIdToProducts < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_10_015310) do
+ActiveRecord::Schema.define(version: 2019_03_12_000425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2019_03_10_015310) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "in_stock"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
