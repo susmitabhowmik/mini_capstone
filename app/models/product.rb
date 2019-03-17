f4d8a6c8f52b80673fdc1df1e7ca4dc2bded2270
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-17 13:32:19 -0400

    create many:many associations between products and categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
   has_many :orders
+  has_many :product_categories
+  has_many :categories, through: :product_categories
 
   validates :name, :price, presence: true
   validates :name, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_15_021450) do
+ActiveRecord::Schema.define(version: 2019_03_17_164858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.string "url"
@@ -29,6 +35,13 @@ ActiveRecord::Schema.define(version: 2019_03_15_021450) do
     t.decimal "subtotal", precision: 9, scale: 2
     t.decimal "tax", precision: 9, scale: 2
     t.decimal "total", precision: 9, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
