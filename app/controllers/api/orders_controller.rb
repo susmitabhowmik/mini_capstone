48afe65195d90e2be7d4bdac8156aa050838ef4c
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-20 20:17:26 -0400

    update orders create action

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -6,21 +6,33 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    #subtotal = quantity * price
-    product = Product.find(params[:product_id])
-    calculated_subtotal = params[:quantity].to_i * product.price
+    @carted_products = CartedProduct.where(user_id: current_user.id, status: "carted")
+
+    calculated_subtotal = 0
+    @carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+    end
+
     tax_rate = 0.08
     calculated_tax = calculated_subtotal * tax_rate
     calculated_total = calculated_subtotal + calculated_tax
 
-    current_user.carted_products.where(status: "carted")
-      @order = Order.new({
-        user_id: current_user.id,
-        subtotal: calculated_subtotal,
-        tax: calculated_tax,
-        total:calculated_total
-      })
-      end
-      
-      render 'show.json.jbuilder'
-      end
+    @order = Order.new(
+    user_id: current_user.id,
+    subtotal: calculated_subtotal,
+    tax: calculated_tax,
+    total: calculated_total )
+    @order.save
+
+    # @carted_products.each do |carted_product|
+    #   carted_product.order_id = @order.id
+    #   carted_product.status = 'purchased'
+    #   carted_product.save
+    # end
+
+    #alternate syntax for above
+    carted_product.update_all(order_id: @order.id, status: 'purchased')
+    render 'show.json.jbuilder'
+  end
+
+end
--- app/views/api/orders/show.json.jbuilder
+++ app/views/api/orders/show.json.jbuilder
@@ -1,6 +1,5 @@
+json.id @order.id
 json.user_id @order.user_id
-json.product_id @order.product_id
-json.quantity @order.quantity
 json.subtotal @order.subtotal
 json.tax @order.tax
 json.total @order.total
