85d4b1a570a7f95bc1a1eab978bee829932e84ea
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-03-24 13:05:50 -0400

    make update and destroy actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,4 +24,27 @@ class ProductsController < ApplicationController
     @product.save
     redirect_to "/products/#{@product.id}"
   end
+
+  def edit
+    @suppliers = Supplier.all
+    @product = Product.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @product.in_stock = params[:in_stock]
+    @product.supplier_id = params[:supplier_id]
+    @product.save
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def delete
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to "/products"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get "/products/new" => "products#new"
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
+  get "/products/:id/edit" => "products#edit"
+  patch "/products/:id" => "products#update"
+  delete "products/:id" => "products#delete"
 
 
   namespace :api do
